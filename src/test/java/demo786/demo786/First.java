204eb173f8b3f11652e7a615ac7f20e2d30f216d
Shanu Anand <shanuanand411@gmail.com>
2025-10-04 18:08:44 +0530

    First comment

---
+++ src/test/java/demo786/demo786/First.java
@@ -0,0 +1,13 @@
+package demo786.demo786;
+
+public class First {
+
+	public static void main(String[] args) {
+		System.out.println("shanu");
+		System.out.println("shanu");
+		System.out.println("shanu");
+		System.out.println("shanu");
+
+	}
+
+}
